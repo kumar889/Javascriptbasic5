20c24391522a994df527e39a352cdfee0ebbb194
kumar889 <kumar.bajura@gmail.com>
2021-06-14 19:18:31 +0545

    Assignment7,commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// using postfix increment operator
+var number_1;
+number_1 = prompt('Enter a number:');
+alert(number_1++);
+
+// using prefix increment operator
+var number_2;
+number_2 = prompt('Enter a number:');
+alert(++number_2);
